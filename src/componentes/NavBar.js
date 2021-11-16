fb0fef3d75454c599f25e15d728671e26a928988
luciano-meloclaps <meloclapsluciano@gmail.com>
2021-11-16 18:44:17 -0300

    lo deje mas lindo

--- src/componentes/NavBar.js
+++ src/componentes/NavBar.js
@@ -3,11 +3,13 @@ import "./NavBar.css";
 const NavBar = () => {
   return (
     <nav className="container">
-      <ul className="container-items">
+      <ul className="container-items1">
         <li>Home</li>
         <li>Catalogo</li>
         <li>Nosotros</li>
         <li>Contacto</li>
+      </ul>
+      <ul className="container-items2">
         <li>Mi Cuenta</li>
       </ul>
     </nav>
