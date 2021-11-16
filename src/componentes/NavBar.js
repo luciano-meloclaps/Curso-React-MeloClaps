dcc4357c5701aedc8111d909e8d085ac6c110ba3
luciano-meloclaps <meloclapsluciano@gmail.com>
2021-11-16 18:06:06 -0300

    Trabajo Clase 3

---
+++ src/componentes/NavBar.js
@@ -0,0 +1,17 @@
+import "./NavBar.css";
+
+const NavBar = () => {
+  return (
+    <nav className="container">
+      <ul className="container-items">
+        <li>Home</li>
+        <li>Catalogo</li>
+        <li>Nosotros</li>
+        <li>Contacto</li>
+        <li>Mi Cuenta</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
